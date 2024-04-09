Fixed TurtlebotDrive classification by adding normalization

scripts/TestModel.py
@@ -5,22 +5,6 @@ import torchvision.transforms as transforms
 from torch.utils.data import DataLoader
 import time
 
-# Check if GPU is available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-# Define transformations
-data_transform = transforms.Compose([
-    transforms.Resize((384, 216)),
-    transforms.ToTensor(),
-])
-
-# Load dataset
-train_dataset = TurtlebotImages(csv_file='training.csv',
-                               image_dir='training',
-                               transform=data_transform)
-
-# Define DataLoader
-train_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
 
 def test_model(model, test_loader, device):
     start_time = time.time()
@@ -30,10 +14,11 @@ def test_model(model, test_loader, device):
     print("Running test")
     with torch.no_grad():
         for images, labels in test_loader:
-            print(images[0].size())
             images, labels = images.to(device), labels.to(device)
             outputs = model(images)
+            # print(outputs)
             _, predicted = torch.max(outputs.data, 1)
+            print(predicted.item())
             total += labels.size(0)
             correct += (predicted == labels).sum().item()
 
@@ -42,10 +27,21 @@ def test_model(model, test_loader, device):
     print(f'Accuracy of the model on the test images: {accuracy:.2f}%')
     print(f"Finished testing, total time taken: {total_time/60.0:.2f} minutes")
 
-model = CNN().to(device)
+if __name__ == '__main__':
+    # Check if GPU is available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-model.load_state_dict(torch.load('model_weights.pth'))
+    # Define transformations
+    data_transform = transforms.Compose([
+        transforms.Resize((384, 216)),
+        transforms.ToTensor(),
+    ])
 
-test_loader = DataLoader(train_dataset, batch_size=32, shuffle=True)
-
-test_model(model, test_loader, device)
+    # Load dataset
+    train_dataset = TurtlebotImages(csv_file='image_velocity_data.csv',
+                                image_dir='images',
+                                transform=data_transform)
+    model = CNN().to(device)
+    model.load_state_dict(torch.load('model_weights.pth'))
+    test_loader = DataLoader(train_dataset, batch_size=1, shuffle=True)
+    test_model(model, test_loader, device)

scripts/TurtlebotDrive.py
@@ -27,6 +27,11 @@ class TurtlebotDrive(Node):
         self.subscription  # prevent unused variable warning
         self.bridge = CvBridge()
         self.current_image = None
+
+        # Initialise velocity and angular velocity
+        # We always start by going straight
+        self.velocity = 1
+        self.ang_velocity = 0
         
         # Load the machine learning model
         # Check if GPU is available
@@ -59,15 +64,23 @@ class TurtlebotDrive(Node):
         # Transpose from RGB to tensor format and add a batch dimension to the start
         image_tensor = torch.from_numpy(np.expand_dims(np.transpose(image, (2, 1, 0)), axis=0)).float()
         image_tensor = image_tensor.to(self.device)
+        # Normalize the tensor
+        # Normalize tensor between 0 and 1
+        min_value = torch.min(image_tensor)
+        max_value = torch.max(image_tensor)
+        image_tensor = (image_tensor - min_value) / (max_value - min_value)
+        # print(image_tensor)
         # Should be [3, 384, 216]
         print(image_tensor.size())
-        prediction = self.model(image_tensor)
-        print(f'prediction is {prediction}')
+        with torch.no_grad():
+            outputs = self.model(image_tensor)
+            print(outputs)
+            _, predicted = torch.max(outputs, 1)
+            print(f'prediction is {predicted}')
 
 def main(args=None):
     rclpy.init(args=args)
     turtlebot = TurtlebotDrive()
-    rate = turtlebot.create_rate(1)
     # Spin in a separate thread
     spin_thread = threading.Thread(target=rclpy.spin, args=(turtlebot, ), daemon=True)
     spin_thread.start()